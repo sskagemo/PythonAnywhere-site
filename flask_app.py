c9d6a8ef153e6219f64d3d255627cc131e1d5899
Steinar Skagemo <elsykkelevangelisten@gmail.com>
2015-11-21 21:22:52 +0000

    reads rdf-triples from web-page using rdflib and prints the number of triples

--- flask_app.py
+++ flask_app.py
@@ -1,11 +1,33 @@
 
-# A very simple Flask Hello World app for you to get started with...
+# Based on the simple Flask Hello World app ...
 
 from flask import Flask
+import rdflib
 
 app = Flask(__name__)
 
+htmldocStart = """
+<html>
+<head>
+<title>Er Tøyenbadet åpent?</title>
+<link rel="stylesheet" type="text/css" href="/s/standard.css" />
+</title>
+<body>
+"""
+htmldocEnd = """
+</body>
+</html>
+"""
+
+#Reading rdf from webpage using rdflib; based on code from http://rdflib.readthedocs.org/en/stable/gettingstarted.html
+g = rdflib.Graph()
+g.parse("https://www.oslo.kommune.no/natur-kultur-og-fritid/svommehaller-i-oslo/toyenbadet/")
+
 @app.route('/')
 def hello_world():
-    return 'Hello from Flask!'
+
+    svar = "Det er " + str(len(g)) + " tripler på siden om Tøyenbadet"
+
+    return htmldocStart + svar + htmldocEnd
+    #'Hello from Flask!'
 
